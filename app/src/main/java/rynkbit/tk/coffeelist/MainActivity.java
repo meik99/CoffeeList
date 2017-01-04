118a28933ea6f08d11ebe064bb09b0f0100606ea
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2017-01-04 18:30:54 +0100

    Changed Font

--- app/src/main/java/rynkbit/tk/coffeelist/MainActivity.java
+++ app/src/main/java/rynkbit/tk/coffeelist/MainActivity.java
@@ -1,12 +1,15 @@
 package rynkbit.tk.coffeelist;
 
 import android.Manifest;
+import android.content.res.Resources;
 import android.os.Build;
 import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.view.View;
 import android.widget.Button;
 
 import java.security.Permission;
@@ -30,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         btnAdminLogin.setOnClickListener(new LoginClickListener(mController));
         userView.setLayoutManager(new GridLayoutManager(this, Constants.GRID_COLUMNS));
         userView.setAdapter(mUserAdapter);
+
     }
 
     @Override
